3b47f82cf481d33f32b3f1e45713b84cf0161f40
jemise111 <jesse.sessler@gmail.com>
2014-05-12 17:50:31 -0400

    implement more collection level functions

--- app/assets/javascripts/backbonefiles/collections/wordCollection.js
+++ app/assets/javascripts/backbonefiles/collections/wordCollection.js
@@ -5,23 +5,35 @@ var WordCollection = Backbone.Collection.extend({
     var curUserID = window.location.pathname.match(/\/\d+$/)[0].replace("/","");
     return "/users/" + curUserID + "/words";
   },
-  inDateTimeRange: function(startDateTime, endDateTime, interval) {
+  graphObjectInDateTimeRange: function(startDateTime, endDateTime, interval) {
     var result = {};
     var timeIntervals = this.timeIntervals(startDateTime, endDateTime, interval);
     for (var i = 0; i < (timeIntervals.length - 1); i++) {
-      var filteredWordArray = this.filter(function(word){
-        var createdAt = new Date(Date.parse(word.get('created_at')));
-        return (createdAt > timeIntervals[i] && createdAt < timeIntervals[i+1]);
-      });
+      var filteredWordArray = this.wordModelsFromStartToEnd(timeIntervals[i], timeIntervals[i+1]);
       result[new Date(timeIntervals[i]).toString()] = filteredWordArray;
     }
     return result;
+  },
+  earliestTimeInRange: function(startDateTime, endDateTime){
+    var filteredWordArray = this.wordModelsFromStartToEnd(startDateTime, endDateTime);
+    var sortedfilteredWordArray = _.sortBy(filteredWordArray, function(word, index){
+      return (new Date(Date.parse(word.get('created_at'))));
+    });
+    var firstWord = sortedfilteredWordArray[0];
+    return new Date(Date.parse(firstWord.get('created_at')));
+  },
+  wordModelsFromStartToEnd: function(startDateTime, endDateTime){
+    var filteredWordArray = this.filter(function(word){
+      var createdAt = new Date(Date.parse(word.get('created_at')));
+      return (createdAt > startDateTime && createdAt < endDateTime);
+    });
+    return filteredWordArray;
   },
   timeIntervals: function(startDateTime, endDateTime, interval){
     var timeIntervals = [];
     var startSecondsSinceEpoch = startDateTime.getTime() / 1000;
     var secondsRange = (endDateTime - startDateTime) / 1000;
-    for (var i = 0; i < secondsRange; i+= 30) {
+    for (var i = 0; i < secondsRange; i+= interval) {
       timeIntervals.push(new Date((startSecondsSinceEpoch + i) * 1000));
     }
     return timeIntervals;
