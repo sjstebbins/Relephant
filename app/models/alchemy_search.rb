2dbfb4b5100f839484eb516c8896ae0702b10e12
jemise111 <jesse.sessler@gmail.com>
2014-05-13 21:50:29 -0400

    add sentiment to alchemy response

--- app/models/alchemy_search.rb
+++ app/models/alchemy_search.rb
@@ -2,7 +2,7 @@ class AlchemySearch
 
   def self.fetch_entities(url)
     alchemy = "9ddce0957447a5027cf1a73b860383823614057f";
-    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=#{url}&apikey=#{alchemy}")
+    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=#{url}&sentiment=1&apikey=#{alchemy}")
   end
 
 end
