996e2cba0c712fbf3c24a81646356bc789550737
jemise111 <jesse.sessler@gmail.com>
2014-05-14 00:51:33 -0400

    testing alchemy search

--- app/models/alchemy_search.rb
+++ app/models/alchemy_search.rb
@@ -1,8 +1,9 @@
 class AlchemySearch
 
   def self.fetch_entities(url)
+    sleep(2)
     alchemy = "9ddce0957447a5027cf1a73b860383823614057f";
-    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=http://www.nytimes.com/2014/05/14/nyregion/newark-mayoral-race.html?hp&_r=0&sentiment=1&apikey=#{alchemy}")
+    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=#{url}&sentiment=1&apikey=#{alchemy}")
   end
 
 end
