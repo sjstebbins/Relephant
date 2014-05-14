71a70a1a7bf9c1f16fc656a24142534f83c3f635
jemise111 <jesse.sessler@gmail.com>
2014-05-14 00:44:17 -0400

    testing alchemy with generic url

--- app/models/alchemy_search.rb
+++ app/models/alchemy_search.rb
@@ -2,7 +2,7 @@ class AlchemySearch
 
   def self.fetch_entities(url)
     alchemy = "9ddce0957447a5027cf1a73b860383823614057f";
-    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=#{url}&sentiment=1&apikey=#{alchemy}")
+    alchemy_response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?outputMode=json&url=http://www.nytimes.com/2014/05/14/nyregion/newark-mayoral-race.html?hp&_r=0&sentiment=1&apikey=#{alchemy}")
   end
 
 end
