6a149f3e7ec4a49f59e9abe5ef20e52ebbddce77
jemise111 <jesse.sessler@gmail.com>
2014-05-14 09:21:29 -0400

    use explicit path for alchemy search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,14 +2,14 @@ class SearchesController < ApplicationController
 
   def show
     @search = Search.find(params[:id])
+    render layout: false
   end
 
   def alchemy_search
     words = params[:words]
     words.gsub!("+", " ")
     search = Search.create(content: words);
-    sleep(2)
-    response = AlchemySearch::fetch_entities(url_for(search))
+    response = AlchemySearch::fetch_entities("http://shrouded-journey-1326.herokuapp.com/searches/#{search.id}");
     # search.destroy
     respond_to do |format|
       format.html { }
