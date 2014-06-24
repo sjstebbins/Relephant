fe0496a47c9d80af04ea1d18145d7cd02ab10d13
spencer stebbins <sjfstebbins@gmail.com>
2014-06-24 10:49:01 -0400

    modals

--- app/assets/javascripts/backbonefiles/views/googleResultsView.js
+++ app/assets/javascripts/backbonefiles/views/googleResultsView.js
@@ -36,6 +36,7 @@ var GoogleResultsView = Backbone.View.extend({
     //       this.$el.append($('<div class="google-result-item"><a class="google-result-link" href="' + link + '">' + title + '</a><p class="google-result-description">' + snippet + '</p></div>'));
     //       });
     //     );
+this.$el.append($('<i class="fa fa-google icons"></i>'));
     _.each(results, function(result, index){
       var title = result.title;
       var snippet = result.snippet;
